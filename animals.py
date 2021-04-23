2c953551639e044d1de09c0cdd68b627a8437f79
Erica Thompson <younique98@gmail.com>
2021-04-23 11:58:43 -0700

    created class construction arguments for the animals

--- animals.py
+++ animals.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class MallardsFish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
@@ -11,202 +11,202 @@ class MallardsFish:
         self.date_added = date.today()
         self.swimming = True
 
-Aimee = MallardsFish()
-Aimee.name = "Miss Aimee"
-Aimee.species = "MarllardsFish"
+Aimee = MallardsFish("Miss Aimee", "MarllardsFish")
+# Aimee.name = "Miss Aimee"
+# Aimee.species = "MarllardsFish"
 print(Aimee)
 
 class Goldfish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.swimming = True
-Nat = Goldfish()
-Nat.name = "Mr Nat"
-Nat.species = "Goldfish"
+Nat = Goldfish("Mr Nat", "Goldfish")
+# Nat.name = "Mr Nat"
+# Nat.species = "Goldfish"
 print(Nat)
 
 class KoiFish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.swimming = True
-Cameron = KoiFish()
-Cameron.name = "Miss Cameron"
-Cameron.species = "Fish"
+Cameron = KoiFish("Miss Cameron", "Fish")
+# Cameron.name = "Miss Cameron"
+# Cameron.species = "Fish"
 print(Cameron)
 
 class CarpsFish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.swimming = True
-Elizabeth = CarpsFish()
-Elizabeth.name = "Miss Elizabeth"
-Elizabeth.species = "Fish"
+Elizabeth = CarpsFish("Miss Elizabeth", "Fish" )
+# Elizabeth.name = "Miss Elizabeth"
+# Elizabeth.species = "Fish"
 print(Elizabeth)
 
 class IdeFish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.swimming = True
-Jack = IdeFish()
-Jack.name = "Mr Jack"
-Jack.species = "Fish"
+Jack = IdeFish("Mr Jack", "Fish")
+# Jack.name = "Mr Jack"
+# Jack.species = "Fish"
 print(Jack)
 
 class CopperheadsSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.slithering = True
-Juliet = CopperheadsSnake()
-Juliet.name = "Miss Juliet"
-Juliet.species = "Snake"
+Juliet = CopperheadsSnake("Miss Juliet", "Snake" )
+# Juliet.name = "Miss Juliet"
+# Juliet.species = "Snake"
 print(Juliet)
 
 class RatSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.slithering = True
-Yvee = RatSnake()
-Yvee.name = "Miss Yvee"
-Yvee.species = "Snake"
+Yvee = RatSnake("Miss Yvee", "Snake")
+# Yvee.name = "Miss Yvee"
+# Yvee.species = "Snake"
 print(Yvee)
 
 class HognoseSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.slithering = True
-Grace = HognoseSnake()
-Grace.name = "Miss Grace"
-Grace.species = "Snake"
+Grace = HognoseSnake("Miss Grace", "Snake" )
+# Grace.name = "Miss Grace"
+# Grace.species = "Snake"
 print(Grace)
 
 class BallPythonSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.slithering = True
-Charlie = BallPythonSnake()
-Charlie.name = "Mr Charlie"
-Charlie.species = "snake"
+Charlie = BallPythonSnake("Mr Charlie", "snake" )
+# Charlie.name = "Mr Charlie"
+# Charlie.species = "snake"
 print(Charlie)
 
 class MilkSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.slithering = True
-Huw = MilkSnake()
-Huw.name = "Mr Huw"
-Huw.species = "domestic snake"
+Huw = MilkSnake("Mr Huw", "domestic snake")
+# Huw.name = "Mr Huw"
+# Huw.species = "domestic snake"
 print(Huw)
 
 class Donkey:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.walking = True
-Paul = Donkey()
-Paul.name = "Mr Paul"
-Paul.species = "Donkey"
+Paul = Donkey("Mr Paul", "Donkey" )
+# Paul.name = "Mr Paul"
+# Paul.species = "Donkey"
 print(Paul)
 
 class Llama:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.walking = True
-Jess = Llama()
-Jess.name = "Miss Fuzz"
-Jess.species = "domestic llama"
+Jess = Llama("Miss Fuzz", "domestic llama" )
+# Jess.name = "Miss Fuzz"
+# Jess.species = "domestic llama"
 print(Jess)
 
 class Goat:
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.walking = True
-Lucy = Goat()
-Lucy.name = "Lucy"
-Lucy.species = "goat"
+Lucy = Goat("Lucy", "goat" )
+# Lucy.name = "Lucy"
+# Lucy.species = "goat"
 print(Lucy)
 print(Lucy.name)
 
 class Goose:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.walking = True
-Oscar = Goose()
-Oscar.name = "Mr Oscar"
-Oscar.species = "domestic goose"
+Oscar = Goose("Mr Oscar", "domestic goose" )
+# Oscar.name = "Mr Oscar"
+# Oscar.species = "domestic goose"
 print(Oscar)
 
 class Pig:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
         self.name = ""
         self.species = ""
         self.date_added = date.today()
         self.walking = True
-Alienor = Pig()
-Alienor.name = "Mr Alienor"
-Alienor.species = "Pig"
+Alienor = Pig("Mr Alienor", "Pig" )
+# Alienor.name = "Mr Alienor"
+# Alienor.species = "Pig"
 print(Alienor)
